17e3421da3fc999f79adfeaed158b9f96dd84e74
systbs <systbs@gmail.com>
2022-10-02 22:17:00 +0300

    add main

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+aba = input('What is your dad name:')
+inp = input('What is your name:')
+
+
+if   inp=="itay" :
+  inp = inp + " the best"
+
+if   inp==aba :
+  inp = inp + " 66"
+
+
+print("Hello, world " + inp)
+
